42fbe4d8f6417980dd2f191309ac15ca00717368
linuskelsey <alfred.linus.kelsey@gmail.com>
2024-02-11 15:18:09 +0000

    Update model building to build all models at once

--- model.py
+++ model.py
@@ -2,17 +2,22 @@ import json
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
-stage = input('Which stage of the game would you like to prepare the model for? ("p" - preflop, "f" - flop, "r" - river or "t" - turn) ')
-while stage not in ['p', 'f', 'r', 't', 'a']:
-    stage = input('Please select one of "p", "f", "r", "t" and "a". ')
+print("Building models for each stage...")
 
 stages = {'p': 'preflop', 'f': 'flop', 'r': 'river', 't': 'turn'}
-stage_file_str = 'game_data/encoded_' + stages[stage] + '.json'
+models = {k: None for k in stages.keys()}
 
-X, Y = [], []
-with open(stage_file_str, 'r') as f:
-    X, Y = json.loads(f.read())
-    f.close()
+for short, stage in stages.items():
+    X, Y = [], []
+    stage_file_str = 'game_data/encoded_' + stage + '.json'
+    with open(stage_file_str, 'r') as f:
+        X, Y = json.loads(f.read())
+        f.close()
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-clf = LogisticRegression().fit(X_train, Y_train)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+    clf = LogisticRegression().fit(X_train, Y_train)
+
+    models[short] = clf
+
+    print(f"\n{stage.title()} model built.")
+    print(f"Parameters: {[round(c, 5) for c in clf.coef_[0]]}")
